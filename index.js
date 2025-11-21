7e50049a5694a0378a1943f68c4c4f081d2f2bbb
jesss-git <jessicachan@Jessicas-MacBook-Pro.local>
2025-11-21 00:04:57 -0500

    Update Construction Component

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { default as Footer } from "./src/components/Footer/Footer.jsx";
 export { default as Navbar } from "./src/components/Navbar/Navbar.jsx";
 export { default as ProjectGallery } from "./src/components/ProjectGallery/ProjectGallery.jsx";
+export { default as UnderConstruction } from "./src/components/UnderConstruction/UnderConstruction.jsx";
