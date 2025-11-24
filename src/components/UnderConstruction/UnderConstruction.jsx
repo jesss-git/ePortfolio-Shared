e44e1513655c09a1e666ee2441056adb0a767dd5
jesss-git <jessicachan@Jessicas-MacBook-Pro.local>
2025-11-24 16:19:18 -0500

    Update Construction Goose

--- src/components/UnderConstruction/UnderConstruction.jsx
+++ src/components/UnderConstruction/UnderConstruction.jsx
@@ -5,6 +5,7 @@ import tape from "../../assets/UnderConstruction/Construction-Tape.png";
 import coming from "../../assets/UnderConstruction/COMING-SOON.png";
 import stay from "../../assets/UnderConstruction/STAY-TUNED!.png";
 import uctext from "../../assets/UnderConstruction/text.png";
+import newGoose from "../../assets/UnderConstruction/NewGoose.png";
 
 export default function UnderConstruction() {
   return (
@@ -12,7 +13,7 @@ export default function UnderConstruction() {
       <div className="uc-stack">
         
         {/* Bottom Layer - Goose */}
-        <img src={goose} alt="Goose" className="uc-goose" />
+        <img src={newGoose} alt="Goose" className="uc-goose" />
         
         {/* Next Layer - Construction Tape */}
         <img src={tape} alt="Construction Tape" className="uc-tape" />
